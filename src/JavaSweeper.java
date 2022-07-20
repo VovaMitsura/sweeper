4a3a13f052b67afc44ea302a8a9d93f6857661f8
Vova <mitsura.vova@lll.kpi.ua>
2022-07-20 13:51:42 +0300

    initial commit

---
+++ src/JavaSweeper.java
@@ -0,0 +1,111 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import sweeper.Box;
+import sweeper.Coord;
+import sweeper.Game;
+import sweeper.Ranges;
+
+public class JavaSweeper extends JFrame {
+
+    private Game game;
+
+    private JPanel panel;
+    private JLabel label;
+
+    private final int COLS = 9;
+    private final int ROWS = 9;
+    private final int IMAGE_SIZE = 50;
+    private final int BOMBS = 10;
+
+    public static void main(String[] args) {
+        new JavaSweeper();
+    }
+
+    private JavaSweeper() {
+        game = new Game(COLS, ROWS, BOMBS);
+        game.start();
+        setImages();
+        initLabel();
+        initPanel();
+        initFrame();
+    }
+
+    private void initPanel() {
+        panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                for (Coord coord: Ranges.getAllCoords()) {
+                    g.drawImage((Image) game.getBox(coord).image, coord.x * IMAGE_SIZE,coord.y * IMAGE_SIZE, this);
+                }
+            }
+        };
+
+        panel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                int x = e.getX() / IMAGE_SIZE;
+                int y = e.getY() / IMAGE_SIZE;
+                Coord coord = new Coord(x, y);
+
+                if(e.getButton() == MouseEvent.BUTTON1){
+                    game.pressLeftButton(coord);
+                }
+
+                if(e.getButton() == MouseEvent.BUTTON3){
+                    game.pressRightButton(coord);
+                }
+
+                if(e.getButton() == MouseEvent.BUTTON2){
+                    game.start();
+                }
+                label.setText(getMassage());
+                panel.repaint();
+            }
+        });
+
+        panel.setPreferredSize(new Dimension(Ranges.getSize().x * IMAGE_SIZE, Ranges.getSize().y * IMAGE_SIZE));
+        add(panel);
+    }
+
+    private String getMassage() {
+        switch (game.getState()){
+            case PLAYED: return "Think twice";
+            case BOMBED: return "YOU LOSE! BIG BA-DA-BOOM!";
+            case WINNER: return "CONGRATULATION!";
+            default: return "WELCOME!";
+        }
+    }
+
+    private void initLabel(){
+        label = new JLabel("Welcome!");
+        add(label, BorderLayout.SOUTH);
+    }
+
+    private void initFrame() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Java Sweeper");
+        setResizable(false);
+        setIconImage(getImage("icon"));
+        setVisible(true);
+        pack();
+        setLocationRelativeTo(null);
+
+    }
+
+    private void setImages() {
+        for (Box box : Box.values()) {
+            box.image = getImage(box.name().toLowerCase());
+        }
+    }
+
+
+    private Image getImage(String name) {
+        String filename = "img/" + name + ".png";
+        ImageIcon icon = new ImageIcon(getClass().getResource(filename));
+        return icon.getImage();
+    }
+}
